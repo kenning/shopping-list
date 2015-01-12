0f36994873469d0042d436a0330ba8806497bebb
kenning <hwerzog@gmail.com>
2015-01-12 09:21:03 -0800

    almost almost done

--- app.js
+++ app.js
@@ -2,26 +2,64 @@
 	var app = angular.module('shoppingList', []);
 
 	app.controller('CategoryController', function() {
+		this.alertMessage = "";
+
 		this.catSupertypes = ['Stores', 'Aisles'];
-		this.supertypeToAdd = "";
+
+		this.supertypeToAdd = {
+			name: "",
+			cats: ['All']
+		};
+
+		this.catToAdd = "";
+		this.addCat = function() {
+			if(this.catToAdd == "") return;
+			this.supertypeToAdd.cats.push(this.catToAdd);
+			this.catToAdd = "";
+		};
+		this.removeCatToAdd = function(catToRemove) {
+			for(var i = 0; i < cats.length; i++){
+				if(cats[i] == catToRemove) {
+					cats.splice(i,1);
+					break;
+				}
+			}
+		};
+
 		this.addSuperType = function() {
-			this.catSuperTypes.push(supertypeToAdd);
-			supertypeToAdd = "";
+			if(this.supertypeToAdd.cats.length < 1) return;
+
+			var newCatName = this.supertypeToAdd.name;
+			this.catSupertypes.push(newCatName);
+
+			this.alertMessage = "Created new category " + newCatName;
+
+			this.cats[this.supertypeToAdd.name] = this.supertypeToAdd.cats;
+
+
+			this.supertypeToAdd = {
+			name: "",
+			cats: ['All']
+			};
+			this.selectedTab = "All";
+			this.alertMessage = "Created new category " + newCatName;
 		};
 
 		this.cats = {
-			Stores: ["Whole foods", "Ralphs"],
-			Aisles: ["Bakery", "Freezer", "Fruits", "Utensils", "Vegetables" ]
+			Stores: ["All", "Whole foods", "Ralphs"],
+			Aisles: ["All", "Bakery", "Freezer", "Fruits", "Utensils", "Vegetables" ]
 		};
 
 		this.selectedTab = "All";
 		this.selectedCategory = false;
 		this.selectTab = function(newTab) {
 			this.selectedTab = newTab;
+			this.alertMessage = "";
 		};
 		this.selectCategory = function(newTab, newCat) {
 			this.selectedTab = newTab;
 			this.selectedCategory = newCat;
+			this.alertMessage = "";
 		};
 	});
 
